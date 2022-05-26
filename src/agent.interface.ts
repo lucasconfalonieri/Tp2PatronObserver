6a4ae41803b1deaf0711b748103cd78f5f7cadf2
Lucas Confalonieri <lucasconfa7.lc@gmail.com>
2022-05-26 17:57:46 -0300

    remove js y index viejo

--- src/agent.interface.ts
+++
@@ -1,7 +0,0 @@
-import { Auctioneer } from "./auctioneer.interface";
-
-export interface Agent {
-  subscribe(auctioneer: Auctioneer): void;
-  unsubscribe(auctioneer: Auctioneer): void;
-  notify(): void;
-}
--- src/auctioneer.interface.ts
+++
@@ -1,7 +0,0 @@
-import { Agent } from "./agent.interface";
-
-export interface Auctioneer {
-  name: string;
-  MAX_LIMIT: number;
-  update(agent: Agent): void;
-}
